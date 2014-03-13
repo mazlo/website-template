6a967cab4d652a98f01dcfa12bc80fc229fd8d4c
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-03-13 16:14:02 +0100

    added contact section

---
+++ app/views/contact.blade.php
@@ -0,0 +1,44 @@
+<div class='contentColumn'>
+
+	<p>
+		<span>Do you have a question</span> and can't find the answer here? <span>Please contact us!</span> 
+	</p>
+	
+	<h4>Contact</h4>
+	
+	<p>
+		Integer <span>facilisis ligula</span> id lacus pretium lobortis. Nunc auctor ornare nisl, <span>ut malesuada</span> orci bibendum nec. 
+		Vestibulum vel magna enim ist <a class='contactDialog' contact='member'>Member Magna</a>.
+	</p>
+
+	<p>
+		Nunc non gravida nibh, vel aliquam metus. <span>Pellentesque hendrerit</span> volutpat velit, 
+		non hendrerit justo laoreet a. Nulla facilisi. <a class='contactDialog' contact='webmaster'>webmaster</a>.
+	</p>
+	
+	<p>
+		<a class='contactDialog' contact='christian'>Member Purus</a> nulla ac sagittis purus, <span>nec malesuada odio</span>. 
+		Aenean consectetur vitae orci et tristique.
+	</p>
+
+	<h4>Further Contact Persons</h4>
+	
+	<p>
+		Integer <span>facilisis ligula</span> id lacus pretium lobortis. Nunc auctor ornare nisl, <span>ut malesuada</span> orci bibendum nec. 
+		Vestibulum vel magna enim. Vestibulum laoreet risus felis, vitae mattis leo <span>auctor eleifend</span>. Nunc elementum mauris purus, 
+		sit amet aliquam justo <span>pharetra eget</span>. Nunc non gravida nibh, vel aliquam metus. <span>Pellentesque hendrerit</span> volutpat velit, 
+		non hendrerit justo laoreet a. Nulla facilisi. Nulla ac sagittis purus, <span>nec malesuada odio</span>. Aenean consectetur vitae orci 
+		et tristique.
+	</p>
+	
+</div>
+
+<div class='imageColumn'>
+	<img src='{{ url( "ct_maschine200x117.jpg" ) }}' border='0' onmouseover='' />
+	<img src='{{ url( "ct_platten200x136.jpg" ) }}' border='0' onmouseover='' />
+	<img src='{{ url( "ct_trickot200x136.jpg" ) }}' border='0' onmouseover='' />
+</div>
+
+<div id='contactDialog' style='display: none'>
+	{{-- ajax content here --}}
+</div>
---
+++ app/views/dialogContact.blade.php
@@ -0,0 +1,31 @@
+<div class='contentColumn'>
+
+	<p>
+		Vitae mattis leo <span>auctor eleifend</span>. Nunc elementum mauris purus, sit amet aliquam justo <span>pharetra eget</span>.
+		Nunc non gravida nibh, vel aliquam metus. 
+	</p>
+			
+	<table class='form-table'>
+		<tbody>
+			<tr>
+
+				<td><b>Subject</b>:</td>
+				<td><input name='subject' id='ssubject' value='' type='text' size='35' class='form-textfield' /></td>
+			
+			</tr>
+			<tr>
+			
+				<td><b>Your E-Mail-Address</b>:</td>
+				<td><input name='semail' id='semail' value='' type='text' size='35' class='form-textfield' /></td>
+			
+			</tr>
+			<tr>
+			
+				<td><b>Your message to {{ $member }}</b>:</td>
+				<td><textarea name='text' rows='15' id='stext' class='form-textarea'></textarea></td>
+			
+			</tr>
+		</tbody>
+	</table>
+
+</div>
