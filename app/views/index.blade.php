a02b909c5321799fd127b5b5fa4b31a6723b77e7
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-03-13 16:01:27 +0100

    added index.php

---
+++ app/views/index.blade.php
@@ -0,0 +1,43 @@
+<div class='contentColumn'>
+	<p>
+		Welcome to the online home of our <span>Table Tennis Club</span>!!
+	</p>
+	
+	<p>
+		Here you will find information <a href='about' class='tabChange'>about us</a>, <a href='training' class='tabChange'>including practice times</a>, 
+		<a href='impressum' class='tabChange'>policies</a>, etc.
+	</p>
+
+	<p>
+		We have <span>experienced coaches</span>, run fun <span>training sessionss</span> and enter teams in the Table Tennis League. 
+		The club is <span>suitable for beginner, intermediate and advanced players</span>.
+	</p>
+	
+	<p>
+		You can find us here in Düsseldorf-Benrath <br />
+		<a target='_blank' href='http://goo.gl/maps/ggz97'>Melanchthonstrasse 2</a><br />
+		40597 Düsseldorf
+	</p>
+
+	<div style='text-align: left'>
+		<h4>Questions?</h4>
+
+		<ul>
+			<li>What are the <a href='training' class='tabChange'>practise hours</a>?</li>
+			<li>Where is the club <a href='training' class='tabChange'>located</a>?</li>
+			<li>When do the <a href='season' class='tabChange'>teams play</a>?</li>
+			<li>I want to <a href='contact' class='tabChange'>ask my own question</a></li>
+		</ul>
+	</div>
+
+	<h4>News</h4>
+
+	<div class='news'>
+		@include( 'news' )
+	</div>
+</div>
+
+<div class='imageColumn'>
+	<img src='{{ url( "ct_zaehlgeraet200x136.jpg" ) }}' />
+	<img src='{{ url( "ct_netz200x136.jpg" ) }}' />
+</div>
