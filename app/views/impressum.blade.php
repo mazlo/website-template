16b8964c489c1b12c0e1e9b6c350e33f4b86e823
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-03-13 16:18:03 +0100

    added disclaimer section

---
+++ app/views/impressum.blade.php
@@ -0,0 +1,13 @@
+<div class="contentColumn">
+
+	<h4>Disclaimer</h4>
+
+	<p>
+		Quisque id odio eu neque imperdiet posuere et a felis. Etiam viverra at massa a aliquet. Morbi odio tellus, 
+		feugiat non nisl et, congue facilisis purus. Cras consectetur mauris imperdiet lorem vehicula auctor. 
+		Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. 
+		Sed facilisis congue justo, a aliquet nisl pellentesque molestie. Aenean mauris dolor, laoreet ut suscipit non, 
+		tincidunt blandit orci.
+	</p>
+
+</div>
