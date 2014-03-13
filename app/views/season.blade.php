388dc53646db77d431881e11b7ff6316bf4e1862
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-03-13 16:17:33 +0100

    wrapped text in a paragraph

--- app/views/season.blade.php
+++ app/views/season.blade.php
@@ -12,10 +12,12 @@
 	
 	<h4>Results</h4>
 
-	Quisque id odio eu neque imperdiet posuere et a felis. Etiam viverra at massa a aliquet. Morbi odio tellus, feugiat non nisl et, 
-	congue facilisis purus. Cras consectetur mauris imperdiet lorem vehicula auctor. Cum sociis natoque penatibus et magnis dis 
-	parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Sed facilisis congue justo, a aliquet nisl pellentesque 
-	molestie. Aenean mauris dolor, laoreet ut suscipit non, tincidunt blandit orci.
+	<p>
+		Quisque id odio eu neque imperdiet posuere et a felis. Etiam viverra at massa a aliquet. Morbi odio tellus, feugiat non nisl et, 
+		congue facilisis purus. Cras consectetur mauris imperdiet lorem vehicula auctor. Cum sociis natoque penatibus et magnis dis 
+		parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Sed facilisis congue justo, a aliquet nisl pellentesque 
+		molestie. Aenean mauris dolor, laoreet ut suscipit non, tincidunt blandit orci.
+	</p>
 
 </div>
 
