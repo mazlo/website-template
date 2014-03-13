8148d172a7c086391dad585fa63a55b0f7e95f10
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-03-13 16:16:24 +0100

    added season section

---
+++ app/views/season.blade.php
@@ -0,0 +1,25 @@
+<div class='contentColumn'>
+
+	<h4>Dates</h4>
+	
+	<p>
+		Integer <span>facilisis ligula</span> id lacus pretium lobortis. Nunc auctor ornare nisl, <span>ut malesuada</span> orci bibendum nec. 
+		Vestibulum vel magna enim. Vestibulum laoreet risus felis, vitae mattis leo <span>auctor eleifend</span>. Nunc elementum mauris purus, 
+		sit amet aliquam justo <span>pharetra eget</span>. Nunc non gravida nibh, vel aliquam metus. <span>Pellentesque hendrerit</span> volutpat velit, 
+		non hendrerit justo laoreet a. Nulla facilisi. Nulla ac sagittis purus, <span>nec malesuada odio</span>. Aenean consectetur vitae orci 
+		et tristique.
+	</p>
+	
+	<h4>Results</h4>
+
+	Quisque id odio eu neque imperdiet posuere et a felis. Etiam viverra at massa a aliquet. Morbi odio tellus, feugiat non nisl et, 
+	congue facilisis purus. Cras consectetur mauris imperdiet lorem vehicula auctor. Cum sociis natoque penatibus et magnis dis 
+	parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Sed facilisis congue justo, a aliquet nisl pellentesque 
+	molestie. Aenean mauris dolor, laoreet ut suscipit non, tincidunt blandit orci.
+
+</div>
+
+<div class='imageColumn'>
+	<img src='{{ url( "ct_netz200x136.jpg" ) }}' />
+	<img src='{{ url( "ct_platten200x136.jpg" ) }}' />
+</div>
