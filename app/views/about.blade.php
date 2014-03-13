f2cc9325b610b76b2b42067b7c0360ed7f7a79f3
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-03-13 16:02:51 +0100

    added about us section

---
+++ app/views/about.blade.php
@@ -0,0 +1,25 @@
+<div class='contentColumn'>
+	<h4>About us</h4>
+	<p>
+		This is a text about us. Welcome!
+	</p>
+	<p>
+		Quisque id odio eu neque imperdiet posuere et a felis. Etiam viverra at massa a aliquet. Morbi odio tellus, <span>feugiat non nisl</span> et, congue facilisis purus. 
+		Cras consectetur mauris imperdiet lorem vehicula auctor. <span>Cum sociis natoque</span> penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+		Aliquam erat volutpat. Sed facilisis <span>congue justo</span>, a aliquet nisl pellentesque molestie. Aenean mauris dolor, laoreet ut suscipit non, tincidunt 
+		blandit orci.
+	</p>
+	<p>
+		Nullam tempor, <span>igula in gravida</span> facilisis, est est venenatis velit, eu suscipit neque risus a nisi. Maecenas nec erat varius, malesuada odio sed, 
+		tincidunt tortor. <span>Cras vel mi tincidunt</span>, porta velit accumsan, semper nibh. Cum sociis natoque penatibus et magnis dis parturient montes, 
+		nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet <span>dictum turpis</span>, in sodales tortor. 
+		Aenean quam lectus, aliquet sit amet molestie ut, auctor vel justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mi dui, 
+		lacinia eu libero a, dictum malesuada nulla. Sed luctus felis ut pretium commodo.
+	</p>
+</div>
+
+<div class='imageColumn'>
+	<img src='{{ url( "ct_platten200x136.jpg" ) }}' />
+	<img src='{{ url( "ct_maschine200x117.jpg" ) }}' />
+	<img src='{{ url( "ct_netz200x136.jpg" ) }}' />
+</div>
