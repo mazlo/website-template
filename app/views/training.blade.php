97cbeaf593e70e8b2da3916a2fb31b4878cd68ce
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-03-13 16:07:39 +0100

    added training section

---
+++ app/views/training.blade.php
@@ -0,0 +1,30 @@
+<div class='contentColumn'>
+	
+	<h4>Location</h4>
+	
+	<p>
+		You can find us here in Düsseldorf-Benrath, <a target='_blank' href='http://goo.gl/maps/ggz97'>Melanchthonstrasse 2</a>, 
+		40597 Düsseldorf
+	</p>
+	
+	<h4>Practise Hours</h4>
+	
+	<p>
+		We practise <span>mondays, wednesdays and fridays from 6pm - 10pm</span>.
+	</p>
+
+	<p>
+		Integer <span>facilisis ligula</span> id lacus pretium lobortis. Nunc auctor ornare nisl, <span>ut malesuada</span> orci bibendum nec. 
+		Vestibulum vel magna enim. Vestibulum laoreet risus felis, vitae mattis leo <span>auctor eleifend</span>. Nunc elementum mauris purus, 
+		sit amet aliquam justo <span>pharetra eget</span>. Nunc non gravida nibh, vel aliquam metus. <span>Pellentesque hendrerit</span> volutpat velit, 
+		non hendrerit justo laoreet a. Nulla facilisi. Nulla ac sagittis purus, <span>nec malesuada odio</span>. Aenean consectetur vitae orci 
+		et tristique.
+	</p>
+	
+</div>
+
+<div class='imageColumn'>
+	<img src='{{ url( "ct_maschine200x117.jpg" ) }}' border='0' onmouseover='' />
+	<img src='{{ url( "ct_platten200x136.jpg" ) }}' border='0' onmouseover='' />
+	<img src='{{ url( "ct_trickot200x136.jpg" ) }}' border='0' onmouseover='' />
+</div>
